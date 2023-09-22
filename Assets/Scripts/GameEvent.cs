85fe56b4ac8a756c8ca1239b994686458310f510
mahiroikuta <ikuta.mahiro@donuts.ne.jp>
2023-09-22 18:00:42 +0900

    update:フォルダ作成

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMain : MonoBehaviour
+public class GameEvent : MonoBehaviour
 {
-    [SerializeField]
-    GameState gameState;
-    [SerializeField]
-    GameEvent gameEvent;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStatus
+{
+    Ready,
+    IsPlaying,
+    GetItem,
+    GameOver,
+    GameClear,
+}
+
+[System.Serializable]
+public class GameState
+{
+    public GameObject player;
+    [System.NonSerialized]
+    public GameStatus gameStatus;
+}
