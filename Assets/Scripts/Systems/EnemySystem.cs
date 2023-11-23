7867264ff0557de7e76a7176b2c8e6c215dc07a5
mahiroikuta <ikuta.mahiro@donuts.ne.jp>
2023-11-23 15:24:53 +0900

    update:キャラチップ追加と敵挙動修正(オブジェクト当たり判定未実装)

--- Assets/Scripts/Systems/EnemySystem.cs
+++ Assets/Scripts/Systems/EnemySystem.cs
@@ -9,12 +9,11 @@ public class EnemySystem
     Vector3 _pPos;
     float _eSpeed;
     private float avoidDistance = 1f;
-    private LayerMask playerLayer = 3;
-    private LayerMask obstacleLayer = 7;
+    private LayerMask playerLayer = 1 << 3;
+    private LayerMask obstacleLayer = 1 << 7;
 
     PlayerComponent _player;
-    EnemyComponent _enemy;
-    EnemyComponent empty;
+    EnemyComponent _enemyComp;
     public EnemySystem(GameState gameState, GameEvent gameEvent)
     {
         _gameState = gameState;
@@ -32,11 +31,11 @@ public class EnemySystem
     void EnemyAction()
     {
         Vector3 pos = _gameState.player.transform.position;
-        foreach (EnemyComponent enemy in _gameState.enemies)
+        foreach (EnemyComponent enemyComp in _gameState.enemies)
         {
-            _enemy = enemy;
-            Vector3 dirToPlayer = (_gameState.player.transform.position - _enemy.transform.position).normalized;
-            _enemy.empty.transform.rotation = Quaternion.LookRotation(dirToPlayer);
+            _enemyComp = enemyComp;
+            Vector3 dirToPlayer = (_gameState.player.transform.position - _enemyComp.transform.position).normalized;
+            _enemyComp.empty.transform.rotation = Quaternion.LookRotation(dirToPlayer);
             if (!ObstacleInPath())
             {
                 MoveTowardsPlayer();
@@ -48,10 +47,7 @@ public class EnemySystem
     {
         RaycastHit hit;
 
-        Vector3 forward = _enemy.empty.transform.TransformDirection(Vector3.forward) * 10;
-        Debug.DrawRay(_enemy.transform.position, forward, Color.blue);
-
-        if (Physics.Raycast(_enemy.transform.position, _enemy.empty.transform.forward, out hit, 2.0f, obstacleLayer))
+        if (Physics.Raycast(_enemyComp.transform.position, _enemyComp.empty.transform.forward, out hit, 2.0f, obstacleLayer))
         {
             Debug.Log("#hit object");
             AvoidObstacle(hit);
@@ -64,17 +60,16 @@ public class EnemySystem
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(_enemy.transform.position, _enemy.empty.transform.forward, out hit, 0.5f, playerLayer))
+        if (Physics.Raycast(_enemyComp.transform.position, _enemyComp.empty.transform.forward, out hit, 0.5f, playerLayer))
         {
-            Debug.Log("#hit player");
-            if (hit.collider.gameObject == _gameState.player.gameObject)
+            if (hit.collider.gameObject == _player.gameObject)
             {
                 EnemyHitPlayer();
             }
         }
         else
         {
-            _enemy.transform.position += _enemy.empty.transform.forward * _eSpeed * Time.deltaTime;
+            _enemyComp.transform.position += _enemyComp.empty.transform.forward * _eSpeed * Time.deltaTime;
         }
     }
 
@@ -82,13 +77,13 @@ public class EnemySystem
     {
         Vector3 avoidDir = Vector3.right * (Random.value > 0.5f ? -avoidDistance : avoidDistance);
 
-        _enemy.transform.position += avoidDir;
-        Vector3 dirToPlayer = (_player.gameObject.transform.position - _enemy.transform.position).normalized;
-        _enemy.empty.transform.rotation = Quaternion.LookRotation(dirToPlayer);
+        _enemyComp.transform.position += avoidDir;
+        Vector3 dirToPlayer = (_player.gameObject.transform.position - _enemyComp.transform.position).normalized;
+        _enemyComp.empty.transform.rotation = Quaternion.LookRotation(dirToPlayer);
     }
 
     void EnemyHitPlayer()
     {
-        _gameEvent.enemyHitPlayer?.Invoke(_enemy.gameObject);
+        _gameEvent.enemyHitPlayer?.Invoke(_enemyComp.gameObject);
     }
 }
